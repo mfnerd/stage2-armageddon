4f4d7d9a2115d94055ff447d20a3a989b62cfa34
Devan Shepard <wdevanshepard@gmail.com>
2025-01-19 18:42:47 -0600

    Update 1-auth.tf

--- 1-auth.tf
+++ 1-auth.tf
@@ -12,5 +12,5 @@ terraform {
 #    region  = "us-east-1"                    # Use a variable for the region
 #    encrypt = true                           # Enable server-side encryption (optional but recommended)
 #   }
-#}
+}
 
