2f31a8da010e0428a20412c7414d33a386613dd1
Devan Shepard <wdevanshepard@gmail.com>
2025-01-19 18:36:04 -0600

    Update 1-auth.tf

--- 1-auth.tf
+++ 1-auth.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket  = "tf-backend-shepzilla"         # Name of the S3 bucket
-    key     = "armageddon-modules-2.tfstate" # The name of the state file in the bucket
-    region  = "us-east-1"                    # Use a variable for the region
-    encrypt = true                           # Enable server-side encryption (optional but recommended)
-  }
-}
+#  backend "s3" {
+#    bucket  = "tf-backend-shepzilla"         # Name of the S3 bucket
+#    key     = "armageddon-modules-2.tfstate" # The name of the state file in the bucket
+#    region  = "us-east-1"                    # Use a variable for the region
+#    encrypt = true                           # Enable server-side encryption (optional but recommended)
+#   }
+#}
 
