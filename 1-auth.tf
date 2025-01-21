6e7a9857e3d1e3681b91466e7d5b192e50f20f1f
Devan Shepard <wdevanshepard@gmail.com>
2025-01-20 21:36:01 -0600

    Update 1-auth.tf

--- 1-auth.tf
+++ 1-auth.tf
@@ -2,11 +2,18 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
     }
   }
+  required_version = ">= 1.1.0"
+
   cloud {
     organization = "MFNERDINCORPORATED"
+
     workspaces {
       name = "stage2"
     }
